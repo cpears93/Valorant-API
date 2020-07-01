31c27e9eff5210d4cde7349de64719c642091aa6
Christopher Pearson <cpearson3140@gmail.com>
2020-07-01 12:52:04 +0100

    Populated model files
    Signed-off-by: Christopher Pearson <cpearson3140@gmail.com>

--- src/main/java/com/example/Valorant/models/Map.java
+++ src/main/java/com/example/Valorant/models/Map.java
@@ -1,4 +1,37 @@
 package com.example.Valorant.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "maps")
 public class Map {
+
+    @Column(name = "Map Name")
+    private String mapName;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @JsonIgnoreProperties(value = "map")
+    @OneToMany(mappedBy = "map", fetch = FetchType.LAZY)
+    private List<Agent> agents;
+
+    public Map(String mapName) {
+        this.mapName = mapName;
+    }
+
+    public Map() {
+    }
+
+    public String getMapName() {
+        return mapName;
+    }
+
+    public void setMapName(String mapName) {
+        this.mapName = mapName;
+    }
 }
